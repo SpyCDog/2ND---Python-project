8fd86476ca39cab54c91d82a12b6290fad695c53
Omrikibubu <omrizitman@gmail.com>
2023-08-19 14:38:30 +0300

    1

--- oz_pro/models.py
+++ oz_pro/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Lead(models.Model):
-    image = models.ImageField(upload_to='static/pics', default = 'Picture7.png')
+    image = models.ImageField(upload_to='static/pics', default = 'static/pics/Picture7.png')
     ID = models.AutoField(primary_key=True, null=False)
     name = models.CharField(max_length=255,null=False)
     phone_number = models.CharField(max_length=15,null=False)
@@ -13,10 +13,10 @@ class Lead(models.Model):
 
 
     def __str__(self):
-        return f'{self.name}, {self.ID}'
+        return f"Name: {self.name}\nID: {self.ID}"
     
 class Customer(models.Model):
-    image = models.ImageField(upload_to='static/pics', default = 'Picture7.png')
+    image = models.ImageField(upload_to='static/pics', default = 'static/pics/Picture7.png')
     ID = models.PositiveIntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15)
@@ -24,20 +24,20 @@ class Customer(models.Model):
     date_of_creation = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Customer Name: {self.name}\nID: {self.ID}\nPhone Number: {self.phone_number}\nType:: {self.customer_type}"
+        return f"Name: {self.name}\nID: {self.ID}\nPhone Number: {self.phone_number}\nType: {self.customer_type}"
 
 class ServicePackage(models.Model):
-    image = models.ImageField(upload_to='static/pics', default = 'pack.png')
+    image = models.ImageField(upload_to='static/pics', default = 'static/pics/pack.png')
     ID = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     package_details = models.TextField()
     date_of_creation = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"Name: {self.name}\nID: {self.ID}"
 
 class Event(models.Model):
-    image = models.ImageField(upload_to='static/pics', default = 'event.png')
+    image = models.ImageField(upload_to='static/pics', default = 'static/pics/event.png')
     ID = models.AutoField(primary_key=True)
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,null=True, related_name='events_for_customer')
     service_package = models.ForeignKey(ServicePackage, on_delete=models.SET_NULL, null=True, related_name='events_for_service_package')
@@ -48,4 +48,4 @@ class Event(models.Model):
     date_of_creation = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"ID: {self.ID}\nCustomer ID: {self.customer}\nService package ID: {self.service_package}"
+        return f"Name: {self.name}\nID: {self.ID}"
